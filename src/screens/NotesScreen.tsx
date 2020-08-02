bd8ff6954dd8d182118267771b88ffee86726b70
Jose Banega <banegajose26@gmail.com>
2020-08-02 19:50:36 -0300

    Create NotesScreen and style

--- src/screens/NotesScreen.tsx
+++ src/screens/NotesScreen.tsx
@@ -3,8 +3,15 @@ import Header from '../components/Header';
 
 const NotesScreen = () => {
   return (
-    <div>
+    <div className="notes--screen">
       <Header></Header>
+      <div className="notes--main">
+        <input type="text" name="title" id="notes--title" className="notes--title" placeholder="Title" />
+        <textarea name="content" id="notes--content" className="notes--content" />
+        <div className="notes--image">
+          <img src="https://media-cdn.tripadvisor.com/media/photo-s/01/1d/ad/c3/orlando.jpg" alt="sunset" />
+        </div>
+      </div>
     </div>
   );
 };
